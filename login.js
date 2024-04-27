c9b435eff391fad7680c946df8fd89f1a1bcabea
redpilledneo <eulinnathaniel@gmail.com>
2024-04-27 19:00:07 +0800

    written most of the tables

---
+++ login.js
@@ -0,0 +1,9 @@
+const connection = require('./shuffle_db')
+
+connection.query("SELECT * from standard_cards where card_suite = 'Hearts'", (error, results, fields) => {
+  if(error){
+    console.error('Error executing SQL query: ', error);
+  } else {
+    console.log('Query results: ', results);
+  }
+})
---
+++ shuffle_db.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+
+// create a new MySQL connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'admajoremDeigloriam!',
+  database: 'shuffle_db'
+});
+
+// connect to the MySQL database
+connection.connect((error) => {
+  if (error) {
+    console.error('Error connecting to MySQL database:', error);
+  } else {
+    console.log('Connected to MySQL database!');
+  }
+});
+
+// export the connection object
+module.exports = connection;
