98f54ab7a53a1cac0717ccb060a16818423e7189
redpilledneo <eulinnathaniel@gmail.com>
2024-05-08 14:27:54 +0800

    added nav bar for both login and signup

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-document.getElementById("but").addEventListener("click", function() {
+document.getElementById("signupButton").addEventListener("click", function() {
   window.location.href = 'signup.html';
 });
+
+document.getElementById("loginButton").addEventListener("click", function() {
+  window.location.href = 'login.html';
+});
+
+
+
