096c598f2339176e4094e02d400e74c027e9e9c5
redpillneo <eulinnathaniel@gmail.com>
2024-05-11 15:17:21 +0800

    signup functionality working on postman

--- signup.js
+++ signup.js
@@ -23,5 +23,5 @@ app.post("/shuffle", async (req, res) => {
 })
 
 app.listen(8080, () => {
-  console.log('Serever is running on port 8080')
+  console.log('Server is running on port 8080 hshs')
 })
