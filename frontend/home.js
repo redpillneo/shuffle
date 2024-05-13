16b1e927e6529a1c7fd14cd50a437d66eab9bd4e
redpilledneo <eulinnathaniel@gmail.com>
2024-05-14 05:32:05 +0800

    home html added

---
+++ frontend/home.js
@@ -0,0 +1,11 @@
+// when I click into the progress html it will redirect into that
+// click to start new session and it will redirect into that
+document.getElementById("progressButton").addEventListener("click", function() {
+  window.location.href = 'progress.html';
+});
+
+document.getElementById("createSessionButton").addEventListener("click", function() {
+  window.location.href = 'config.html';
+});
+
+
