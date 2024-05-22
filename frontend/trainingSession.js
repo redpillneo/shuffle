1f633f4934e3cdf59384c042761784e308d7b084
redpillneo <eulinnathaniel@gmail.com>
2024-05-22 12:59:32 +0800

    training session navigation fixed

--- frontend/trainingSession.js
+++ frontend/trainingSession.js
@@ -33,7 +33,7 @@ console.log(card_no)
 
 for(var i = 1; i <= deck_no; i++){
   var deck = []
-  for(var j = i; j <= card_no; j++){
+  for(var j = 1; j <= card_no; j++){
     var randomCard = pickRandomCard(standard_deck)
     while(isCardInDeck(randomCard, deck)){
       randomCard = pickRandomCard(standard_deck)
@@ -76,8 +76,8 @@ window.onload = () => {
 // generating the images from the sessionCards
 document.getElementById('deck-right').addEventListener('click', goToNextDeck)
 document.getElementById('deck-left').addEventListener('click', backToLastDeck)
-// document.getElementById('shift-to-top').addEventListener('click', shiftToTop)
-// document.getElementById('shift-top-bottom').addEventListener('click', shiftToBottom)
+document.getElementById('shift-to-top').addEventListener('click', shiftToTop)
+document.getElementById('shift-to-bottom').addEventListener('click', shiftToBottom)
 // document.getElementById('retract').addEventListener('click', retract)
 // document.getElementById('peek').addEventListener('click', peek)
 
@@ -99,6 +99,7 @@ function createCard(card, index){
 
   cardDiv.appendChild(img)
   cardContainer.appendChild(cardDiv)
+  displayCard(0)
 }
 
 // generateCards(sessionCards, deckIndex)
@@ -110,41 +111,92 @@ function generateDeck(sessCard, dIndex){
 
 document.getElementById('deckNoVal').innerHTML = `${deckIndex+1}/${deck_no}`
 document.getElementById('cardNoVal').innerHTML = `${cardIndex+1}/${card_no}`
-generateDeck(sessionCards,deckIndex)
 // function for each event listener
 // move from one deck to the other
 function goToNextDeck(){
+  cardIndex = 0
+  displayCard(cardIndex);
   if(deckIndex < (sessionCards.length-1)){
     cardContainer.innerHTML = ''
     deckIndex++
+    console.log(deckIndex)
     generateDeck(sessionCards, deckIndex)
     document.getElementById('deckNoVal').innerHTML = `${deckIndex+1}/${deck_no}`
   }
 }
 // backToLastDeck
 function backToLastDeck(){
-  if(deckIndex > 0){
+  if(deckIndex >= 1){
     cardContainer.innerHTML = ''
     deckIndex--
+    console.log("the deck index is", deckIndex)
     generateDeck(sessionCards, deckIndex)
     document.getElementById('deckNoVal').innerHTML = `${deckIndex+1}/${deck_no}`
   }
+  displayCard(0)
 }
 
+function displayCard(index) {
+  const cards = document.querySelectorAll('.card')
+  cards.forEach((card, i) => {
+    card.style.display = (i === index) ? 'block' : 'none';
+  })
+}
 
+// function displayCard(index) {
+//   console.log("displayCard called")
+//   console.log(cards.length)
+// }
+// displaying the cards
+// function displayCard(index){
+//   for(var i = 1; i < index; i++){
+//     cards[i].style.display
+//   }
+// }
+
+
+
+function shiftToBottom() {
+  if (cardIndex < sessionCards[deckIndex].length-1) {
+    cardIndex++;
+    displayCard(cardIndex);
+  }
+}
+function shiftToTop() {
+  if (cardIndex > 0) {
+    cardIndex--;
+    displayCard(cardIndex);
+  }
+}
 // goToNextDeck
 document.addEventListener('keydown', (event) => {
+  displayCard(0)
   if(event.key === "ArrowRight" && event.altKey){
     goToNextDeck()
   }
 });
 // backToLastDeck
 document.addEventListener('keydown', (event) => {
+  displayCard(0)
   if(event.key === "ArrowLeft" && event.altKey){
     backToLastDeck()
   }
 });
 
+// shiftcard
+document.addEventListener('keydown', (event) => {
+  if(event.key === "ArrowRight" && event.shiftKey){
+    shiftToBottom()
+  }
+})
+
+document.addEventListener('keydown', (event) => {
+  if(event.key === "ArrowLeft" && event.shiftKey){
+    shiftToTop()
+  }
+})
+
+generateDeck(sessionCards, deckIndex)
 
 // for(var i = 1; i <= deck_no; i++){
 //   console.log("deck ", i)
